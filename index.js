b03a3fdae46cfd133a54ade88b53db5f81b0a040
sohan24-dev <sohan.explorer@gmail.com>
2026-05-13 01:52:36 +0600

    add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors())
