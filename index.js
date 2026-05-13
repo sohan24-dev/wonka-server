238b2fb66abb073cc17d5123257c21a50475952b
sohan24-dev <sohan.explorer@gmail.com>
2026-05-14 01:45:22 +0600

    add

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors())
+app.use(cors({
+    origin: "https://wonka-restaurant.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json())
 
 const uri = process.env.MONGODB_URI;
