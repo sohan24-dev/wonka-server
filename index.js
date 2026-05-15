d23029fdca6dc6c2e740377d407b9fb6af40f23b
sohan24-dev <sohan.explorer@gmail.com>
2026-05-15 18:03:59 +0600

    add

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json())
 
@@ -27,34 +26,31 @@ const jwks = createRemoteJWKSet(
 )
 
 const verifytoken = async (req, res, next) => {
-    const headerss = req?.headers?.authorization;
-    if (!headerss) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
         return res.status(401).json({
-            massage: "Unauthorized"
+            message: "Unauthorized"
         })
     }
-    const token = headerss.split(' ')[1];
+    const token = authHeader.split(' ')[1];
     if (!token) {
         return res.status(401).json({
-            massage: "Unauthorized"
+            message: "Unauthorized"
         })
     }
     try {
         const { payload } = await jwtVerify(token, jwks)
-        console.log(payload);
+        req.user = payload;
         next()
     } catch (error) {
         return res.status(403).json({
-            massage: "Forbidden"
+            message: "Forbidden"
         })
     }
-
 }
-
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const database = client.db("userData");
         const data = database.collection("itemsData");
@@ -65,9 +61,9 @@ async function run() {
             const allValues = await cursor.toArray();
             res.send(allValues)
         })
+
         app.get('/data/:id', verifytoken, async (req, res) => {
             const id = req.params.id;
-            // console.log(id);
             const cursor = await data.findOne({ _id: new ObjectId(id) })
             res.send(cursor)
         })
@@ -77,33 +73,33 @@ async function run() {
             const result = await orderlist.insertOne(doc)
             res.send(result)
         })
+
         app.get('/orderlist', async (req, res) => {
             const cursor = await orderlist.find()
             const allValues = await cursor.toArray();
             res.send(allValues)
         })
+
         app.get('/orderlist/:id', verifytoken, async (req, res) => {
             const id = req.params.id;
-            // console.log(id);
             const cursor = await orderlist.findOne({ _id: new ObjectId(id) })
             res.send(cursor)
         })
+
         app.delete('/orderlist/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const cursor = await orderlist.deleteOne(query)
-            // console.log(cursor);
             res.send(cursor)
         })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+    } catch (error) {
+        console.error(error);
     }
 }
 run().catch(console.dir);
-
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
